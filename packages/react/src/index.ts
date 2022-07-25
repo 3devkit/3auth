717fede3f68e1fef7d7c8e9f981fc332f627b5c7
tangzijun <tangzijun@keypod.cn>
2022-07-25 13:56:39 +0800

    fix

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -2,7 +2,9 @@ export * from './provider';
 export * from './use-loginState';
 export * from './use-myInfo';
 
+export * from '@3auth/core';
 export * from '@3auth/auth';
+export * from '@3auth/react-ui';
 
 export { MetamaskConnector } from '@3walletconnector/wallet-metamask';
 export { PhantomConnector } from '@3walletconnector/wallet-phantom';
